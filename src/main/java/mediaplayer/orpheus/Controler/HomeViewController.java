2113d6eb009a03da65a4e56d4be6b4f783bcdbb2
kamapp01 <148754033+kamapp01@users.noreply.github.com>
2024-01-10 20:18:11 +0100

    skipForward + skipBackward and currentLabel work

--- src/main/java/mediaplayer/orpheus/Controler/HomeViewController.java
+++ src/main/java/mediaplayer/orpheus/Controler/HomeViewController.java
@@ -1,9 +1,11 @@
 package mediaplayer.orpheus.Controler;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.util.Duration;
 import mediaplayer.orpheus.Controler.ViewControler;
 
 import java.io.IOException;
@@ -15,34 +17,56 @@ import javafx.scene.media.MediaView;
 import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Timer;
+import java.util.TimerTask;
 
-    public class HomeViewController implements Initializable {
+public class HomeViewController implements Initializable {
 
         @FXML
         private MediaView mediaViewDisplay;
+
+        @FXML
+        private Label labCurrentTime;
+
+        @FXML
+        private Label labMediaLength;
+
 
         private File file;
         private Media media;
         private MediaPlayer mediaPlayer;
         private int playSwitchStage = 0;
+        private Timer timer;
+        private TimerTask task;
+        private double current;
+
 
 
         @Override
         public void initialize(URL url, ResourceBundle resourceBundle) {
 
-            file = new File("src/main/java/mediaplayer/orpheus/mediaFiles/Bryan Adams - Summer Of  69 (Official Music Video).mp4");
+            file = new File("src/main/java/mediaplayer/orpheus/mediaFiles/CAN T STOP THE FEELING! (from DreamWorks Animation s  TROLLS ) (Official Video).mp4");
 
             if (file.exists()) {
                 media = new Media(file.toURI().toString());
                 mediaPlayer = new MediaPlayer(media);
             } else {
-                System.out.println("Filen blev ikke fundet på den specificerede sti.");
+                System.out.println("The file was not found at the specified path");
             }
+
+            // when the MediaPlayer is ready to play the media
+            mediaPlayer.setOnReady(() -> {
+                double trackLength = media.getDuration().toSeconds();
+                String formattedTime = secondsFormattedToTime(trackLength);
+                labCurrentTime.setText(formattedTime);
+            });
 
             mediaViewDisplay.setMediaPlayer(mediaPlayer);
         }
 
         public void onBtnPlayClick(){
+
+            beginTimer();
 
             if (playSwitchStage == 0) {
                 mediaPlayer.play();
@@ -57,7 +81,76 @@ import java.util.ResourceBundle;
 
 
 
+        public void beginTimer(){
 
+            timer = new Timer();
+            task = new TimerTask() {
+                @Override
+                public void run() {
+
+                    Platform.runLater(() -> {
+                        if (mediaPlayer != null) {
+                            current = mediaPlayer.getCurrentTime().toSeconds();
+                            double trackLength = media.getDuration().toSeconds();
+                            String formattedTime = secondsFormattedToTime(current);
+                            labCurrentTime.setText(formattedTime);
+
+                            labMediaLength.setText(secondsFormattedToTime(trackLength));
+                        }
+                    });
+                }
+            };
+
+            timer.scheduleAtFixedRate(task,1000,1000);
+        }
+
+
+        public void cancelTimer(){
+
+        }
+
+
+
+        public static String secondsFormattedToTime(double durationTime) {
+            //test: durationTime = 5420;
+            int hours = (int) durationTime / 3600;
+            int secondsLeft = (int) durationTime % 3600;
+            int minutes = secondsLeft / 60;
+            int seconds = secondsLeft % 60;
+
+            return String.format("%02d:%02d:%02d", hours, minutes, seconds);
+        }
+
+
+
+        public void onBtnSkipForwardsClick(){
+
+            System.out.println("current time: "+current);
+            current = mediaPlayer.getCurrentTime().toSeconds();
+            int forwardTime = (int) current + 15;
+
+            updateCurrentTimeLabel(forwardTime);
+
+            mediaPlayer.seek(Duration.seconds(forwardTime));
+            mediaPlayer.play();
+        }
+
+
+        public void onBtnSkipBackwardsClick(){
+            current = mediaPlayer.getCurrentTime().toSeconds();
+            current = mediaPlayer.getCurrentTime().toSeconds();
+            int forwardTime = (int) current - 15;
+
+            updateCurrentTimeLabel(forwardTime);
+
+            mediaPlayer.seek(Duration.seconds(forwardTime));
+            mediaPlayer.play();
+        }
+
+        private void updateCurrentTimeLabel(int time) {
+
+            labCurrentTime.setText(secondsFormattedToTime(time));
+        }
 
 
 
