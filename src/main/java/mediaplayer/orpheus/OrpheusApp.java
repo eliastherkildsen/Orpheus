58f5b3798f3add01e1ad3ab23e2b803792a645ba
Elias Therkildsen <73726902+eliastherkildsen@users.noreply.github.com>
2024-01-11 13:27:20 +0100

    removed unused var in App.java

--- src/main/java/mediaplayer/orpheus/OrpheusApp.java
+++ src/main/java/mediaplayer/orpheus/OrpheusApp.java
@@ -10,8 +10,6 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class OrpheusApp extends Application {
-    private final int SCREEN_WITH = 800;
-    private final int SCREEN_HIGHT = 800;
     @Override
     public void start(Stage stage) throws IOException {
         try {
