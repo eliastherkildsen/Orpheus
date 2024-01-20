0139ff75483d15e31b52f64b8b71477fa0c5c361
Bolt404 <mads@rknudsen.dk>
2024-01-20 23:20:46 +0100

    Further Improvements to GUI

--- src/main/java/mediaplayer/orpheus/Controler/HomeViewController.java
+++ src/main/java/mediaplayer/orpheus/Controler/HomeViewController.java
@@ -177,7 +177,7 @@ public class HomeViewController implements Initializable {
         homePane.heightProperty().addListener(new ChangeListener<Number>() {
             @Override
             public void changed(ObservableValue<? extends Number> observableValue, Number number, Number t1) {
-                double padding = 40;
+                double padding = 80;
                 mediaViewDisplay.setFitHeight(t1.doubleValue() - padding);
 
                 //Saving the height every time its changed for later use.
