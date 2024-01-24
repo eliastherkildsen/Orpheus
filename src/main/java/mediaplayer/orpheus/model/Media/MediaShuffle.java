dddd4ffa4b4c58fd26456b8617ab9c0ca0f7b668
kamapp01 <148754033+kamapp01@users.noreply.github.com>
2024-01-24 15:27:11 +0100

    homeView refactored + MediaShuffle and MediaSkip

--- src/main/java/mediaplayer/orpheus/model/Media/MediaShuffle.java
+++ src/main/java/mediaplayer/orpheus/model/Media/MediaShuffle.java
@@ -5,16 +5,24 @@ import java.util.Collections;
 
 public class MediaShuffle {
 
+    /**
+     * Method for shuffle the chosen playlist.
+     * This method takes a chosen playlist, shuffles its order, and returns the shuffled playlist.
+     *
+     * @param playlist the chosen playlist to be shuffled
+     * @return the shuffled playlist
+     */
     public static ArrayList<MediaObj> shufflePlaylist (ArrayList<MediaObj> playlist){
+        // checks the size of the playlist
+        //System.out.println(playlist.size());
 
-        System.out.println(playlist.size());
-
+        // shuffles the playlist
         Collections.shuffle(playlist);
 
-        for (MediaObj obj : playlist){
-
+        // prints the shuffled playlist
+        /*for (MediaObj obj : playlist){
         System.out.println("\nShuffled List : \n" + obj.getMediaTitle());
-        }
+        }*/
 
         return playlist;
     }
